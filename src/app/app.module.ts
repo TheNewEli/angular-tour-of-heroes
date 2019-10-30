63739a53800c95cb89c3ee6ce4b881b5efc4f4ce
caoyu <caoyupersonal@hotmail.com>
2019-10-30 21:52:45 +0800

    Routing section completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,4 +17,11 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message_after_fetching the heroes
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    console.log(id);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
